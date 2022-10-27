e26a42e7973810ddb4030e17cfc5603fe1fce556
Devin Monro <mage1d@yahoo.com>
2022-10-27 18:21:33 -0400

    Mount Checking

---
+++ README.md
@@ -0,0 +1,3 @@
+# RedM event handler
+
+Vehicle Check Code snippets used from : https://github.com/citizenfx/cfx-server-data/blob/master/resources/%5Bsystem%5D/baseevents/vehiclechecker.lua
--- client.lua
+++ client.lua
@@ -9,6 +9,8 @@ local GetSeatPedIsTryingToEnter = GetSeatPedIsTryingToEnter
 local IsPedInAnyVehicle = IsPedInAnyVehicle
 local VehToNet = VehToNet
 local IsPedOnMount = IsPedOnMount
+local GetMount = GetMount
+local NetworkGetNetworkIdFromEntity = NetworkGetNetworkIdFromEntity
 
 local GetNumberOfEvents = GetNumberOfEvents
 local GetEventAtIndex = GetEventAtIndex
@@ -48,6 +50,8 @@ end)
 
 CreateThread(function()
 	while true do
+
+        -- RD2 Events : https://github.com/femga/rdr3_discoveries/blob/master/AI/EVENTS/events.lua
         local size = GetNumberOfEvents(0)
         if size > 0 then
             for i = 0, size - 1 do
@@ -63,14 +67,17 @@ CreateThread(function()
         end
 
         local ped = PlayerPedId()
+        local id = PlayerId()
 
-        if not IN_VEHICLE and not IsPlayerDead(PlayerId()) then
+        -- Vehicles
+        if not IN_VEHICLE and not IsPlayerDead(id) then
 			if DoesEntityExist(GetVehiclePedIsEntering(ped)) and not ENTERING_VEHICLE then
 				-- trying to enter a vehicle!
 				local vehicle = GetVehiclePedIsEntering(ped)
 				local seat = GetSeatPedIsTryingToEnter(ped)
 				local netId = VehToNet(vehicle)
 				ENTERING_VEHICLE = true
+                TriggerEvent('events:EnteringVehicle', vehicle, seat, netId)
 				TriggerServerEvent('events:EnteringVehicle', vehicle, seat, netId)
                 --print(string.format("ATTEMPTING: %s %s %s", vehicle, seat, netId))
 			elseif not DoesEntityExist(GetVehiclePedIsEntering(ped)) and not IsPedInAnyVehicle(ped, true) and ENTERING_VEHICLE then
@@ -87,10 +94,11 @@ CreateThread(function()
 				local model = GetEntityModel(CURRENT_VEHICLE)
 				local netId = VehToNet(CURRENT_VEHICLE)
                 --print(string.format("ENTERED: %s %s %s", CURRENT_VEHICLE, CURRENT_SEAT, netId))
+                TriggerEvent('events:EnteredVehicle', CURRENT_VEHICLE, CURRENT_SEAT, netId)
 				TriggerServerEvent('events:EnteredVehicle', CURRENT_VEHICLE, CURRENT_SEAT, netId)
 			end
 		elseif IN_VEHICLE then
-			if not IsPedInAnyVehicle(ped, false) or IsPlayerDead(PlayerId()) then
+			if not IsPedInAnyVehicle(ped, false) then
 				-- bye, vehicle
 				local model = GetEntityModel(CURRENT_VEHICLE)
 				local netId = VehToNet(CURRENT_VEHICLE)
@@ -102,7 +110,24 @@ CreateThread(function()
 			end
 		end
 
-
+        -- Mounts
+        if not ON_HORSE and not IsPlayerDead(id) then
+            if IsPedOnMount(ped) then
+                CURRENT_HORSE = GetMount(ped)
+                ON_HORSE = true
+                local netId = NetworkGetNetworkIdFromEntity(CURRENT_HORSE)
+                TriggerClientEvent('events:MountOn', netId)
+                TriggerServerEvent('events:MountOn', netId)
+            end
+        elseif ON_HORSE then
+            if not IsPedOnMount(ped) then
+                local netId = NetworkGetNetworkIdFromEntity(CURRENT_HORSE)
+                TriggerClientEvent('events:MountOff', netId)
+                TriggerServerEvent('events:MountOff', netId)
+                ON_HORSE = false
+                CURRENT_HORSE = nil
+            end
+        end
 
         Wait(0)
 	end
